6fafa52380ff557b9b36a00dcff341dbf36eb481
Mayank Porwal <34401313+Mynk14@users.noreply.github.com>
2020-07-15 12:15:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -89,10 +89,10 @@ app = webapp2.WSGIApplication([
 ], debug=True)
 
 
-def main():
-    from paste import httpserver
-    httpserver.serve(app, host='127.0.0.1', port='8080')
+# def main():
+#     from paste import httpserver
+#     httpserver.serve(app, host='127.0.0.1', port='8080')
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
