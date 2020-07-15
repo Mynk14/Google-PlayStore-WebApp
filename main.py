dffb5f12c33a39e7fa56ae25c203454e1d0b15d5
Mayank Porwal <34401313+Mynk14@users.noreply.github.com>
2020-07-15 17:27:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,11 +26,12 @@ for i in top_free_apps:
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
         self.response.write("""
+<!doctype html>
 <html>
 <head>
-        <link type="text/css" rel="stylesheets" href="/stylesheets/style.css" />
-        <style>
-        body{
+        <meta charset="utf-8">
+	<style>
+	body{
 	background-image: url("https://i.pinimg.com/originals/2f/3c/dc/2f3cdc6a9b182457f81b4f72efd26ab3.jpg"); 
 }
 #a {
@@ -46,7 +47,7 @@ class HelloWebapp2(webapp2.RequestHandler):
 	padding-top:20px;
 	padding-bottom: 0px;
 	background-color:  #00001a;
-	box-shadow: 10px 10px black;
+	/*box-shadow: 10px 10px black;*/
 	border-radius: 25px; 
 }
 #c{
@@ -59,32 +60,113 @@ class HelloWebapp2(webapp2.RequestHandler):
 	text-shadow: 2px 2px 4px #000000;
 }
 #d{
+	margin-left: 90px;
 	color:#507c7c;
 	font-size: 30px;
 	padding-left: 50px;
 }
 #e{
+	margin-left: 90px;
 	color:#507c7c;
 	font-size: 30px;
 	padding-left: 50px;
 }
 #f{
+	margin-left: 90px;
 	color:#507c7c;
 	font-size: 30px;
 	padding-left: 50px;
 }
+#app1{
+	margin-left: 200px;
+	margin-right: 200px;
+	float:right;
+	text-align:top;
+	border-width:2px;
+	border-color:red;
+	height:150px;
+	width : 150px;
+	color : white;
+	text-align: center;
+}
+#app2{
+	margin-left: 200px;
+	float:right;
+	text-align:top;
+	border-width:2px;
+	border-color:red;
+	height:150px;
+	width : 150px;
+	color : white;
+	text-align: center;
+}
+#app3{
+	float:right;
+	text-align:top;
+	border-width:2px;
+	border-color:red;
+	height:150px;
+	width : 150px;
+	color : white;
+	text-align: center;
+}
 </style>
-	<meta charset="utf-8">
 	<title>WebApp</title>
-
 </head>
 <body>
 	<h1 id="a"><img src="https://www.americares.org/wp-content/uploads/refresh.png" width="30px" height="30px"></h1>
 	<h1 id="b"><a href="https://www.bluestacks.com/" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/en/0/04/BlueStacks_Logo.png" width="240px" height="65px"></a></h1>
-	<h2 id="c">Top Charts</h2>
-	<div id = "d">Top Paid</div><br><br>
-	<div id = "e">Top Downloaded</div><br><br>
+	<h2 id="c">Top Charts</h2><br>
+	<div id = "d">Top Paid</div><br>
+	<p id = "app1">
+		<img id = "app1_img" src = "" width = "90px" height="90px"><br><br>
+		Club Factory<br>
+		Club Factory Inc.
+	</p>
+	<p id = "app2">
+		<img id = "app2_img" src = "" width = "90px" height="90px"><br><br>
+		Whatsapp<br>Whatsapp Inc.
+	</p>
+	<p id = "app3">
+		<img id = "app3_img" src = "" width = "90px" height="90px"><br><br>
+		Hotstar<br>
+		Disney
+
+	</p>
+	<br><br><br><br><br><br><br><br><br><br><br><br>
+	<div id = "e">Top Reviewed</div><br><br>
+	<p id = "app1">
+		<img id = "app1_img" src = "" width = "90px" height="90px"><br><br>
+		Club Factory<br>
+		Club Factory Inc.
+	</p>
+	<p id = "app2">
+		<img id = "app2_img" src = "" width = "90px" height="90px"><br><br>
+		Whatsapp<br>Whatsapp Inc.
+	</p>
+	<p id = "app3">
+		<img id = "app3_img" src = "" width = "90px" height="90px"><br><br>
+		Hotstar<br>
+		Disney
+
+	</p>
+	<br><br><br><br><br><br><br><br><br><br><br><br>
 	<div id = "f">Top Rated</div><br><br>
+	<p id = "app1">
+		<img id = "app1_img" src = "" width = "90px" height="90px"><br><br>
+		Club Factory<br>
+		Club Factory Inc.
+	</p>
+	<p id = "app2">
+		<img id = "app2_img" src = "" width = "90px" height="90px"><br><br>
+		Whatsapp<br>Whatsapp Inc.
+	</p>
+	<p id = "app3">
+		<img id = "app3_img" src = "" width = "90px" height="90px"><br><br>
+		Hotstar<br>
+		Disney
+
+	</p>
 </body>
 </html>""")
 
