df4b3a9e8e388ee0bfb0d8a05d8094c0bb4d3ff2
Mayank Porwal <34401313+Mynk14@users.noreply.github.com>
2020-07-19 22:08:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import urllib2
 from google.appengine.ext import db
 
 
-class App(db.Model):
+class TopFree(db.Model):
     app_img = db.StringProperty(required=True)
     app_name = db.StringProperty(required=True)
     app_pub = db.StringProperty(required=True)
@@ -20,7 +20,8 @@ class App(db.Model):
 # def App_key(app_package=None):
 #   return db.Key.from_path('App', app_package)
 
-response = urllib2.urlopen('')
+response = urllib2.urlopen('https://play.google.com/store/apps/collection/cluster?clp=0g4jCiEKG3RvcHNlbGxpbmdfZnJlZV9BUFBMSUNBVElPThAHGAM%3D:S:ANO1ljKs-KA&gsr=CibSDiMKIQobdG9wc2VsbGluZ19mcmVlX0FQUExJQ0FUSU9OEAcYAw%3D%3D:S:ANO1ljL40zU&hl=en_IN')
+#response = urllib2.urlopen('https://play.google.com/store/apps/top?hl=en_IN')
 html_content = response.read()
 soup = BeautifulSoup(html_content, "lxml")
 top_free_apps = soup.find_all(attrs={"class": "ImZGtf mpg5gc"})
@@ -29,11 +30,55 @@ for i in top_free_apps:
     name = i.find_all(attrs={"class": "WsMG1c"})
     pub = i.find_all(attrs={"class": "KoLSrc"})
     package = i.find_all(attrs={"class": "poRVub"})
-    obj = App(app_img=img[0]["data-src"], app_name=name[0].text, app_pub=pub[0].text[:20],
+    obj = TopFree(app_img=img[0]["data-src"], app_name=name[0].text, app_pub=pub[0].text[:20],
               app_package=package[0]["href"][23:])
     obj.put()
     # print(app_img[0]["data-src"])
     # print(app_name[0].text)
+
+
+class TopGross(db.Model):
+    app_img = db.StringProperty(required=True)
+    app_name = db.StringProperty(required=True)
+    app_pub = db.StringProperty(required=True)
+    app_package = db.StringProperty(required=True)
+
+
+response2 = urllib2.urlopen('https://play.google.com/store/apps/collection/cluster?clp=0g4fCh0KF3RvcGdyb3NzaW5nX0FQUExJQ0FUSU9OEAcYAw%3D%3D:S:ANO1ljLe6QA&gsr=CiLSDh8KHQoXdG9wZ3Jvc3NpbmdfQVBQTElDQVRJT04QBxgD:S:ANO1ljKx5Ik&hl=en_IN')
+#response2 = urllib2.urlopen('https://play.google.com/store/apps/top?hl=en_IN')
+html_content2 = response2.read()
+soup2 = BeautifulSoup(html_content2, "lxml")
+top_grossing_apps = soup2.find_all(attrs={"class": "ImZGtf mpg5gc"})
+for i in top_grossing_apps:
+    img = i.find_all("img")
+    name = i.find_all(attrs={"class": "WsMG1c"})
+    pub = i.find_all(attrs={"class": "KoLSrc"})
+    package = i.find_all(attrs={"class": "poRVub"})
+    obj2 = TopGross(app_img=img[0]["data-src"], app_name=name[0].text, app_pub=pub[0].text[:20],
+              app_package=package[0]["href"][23:])
+    obj2.put()
+
+
+class TopGames(db.Model):
+    app_img = db.StringProperty(required=True)
+    app_name = db.StringProperty(required=True)
+    app_pub = db.StringProperty(required=True)
+    app_package = db.StringProperty(required=True)
+
+
+response3 = urllib2.urlopen('https://play.google.com/store/apps/collection/cluster?clp=0g4YChYKEHRvcGdyb3NzaW5nX0dBTUUQBxgD:S:ANO1ljLhYwQ&gsr=ChvSDhgKFgoQdG9wZ3Jvc3NpbmdfR0FNRRAHGAM%3D:S:ANO1ljIKta8&hl=en_IN')
+#response3 = urllib2.urlopen('https://play.google.com/store/apps/top?hl=en_IN')
+html_content3 = response3.read()
+soup3 = BeautifulSoup(html_content3, "lxml")
+top_games = soup3.find_all(attrs={"class": "ImZGtf mpg5gc"})
+for i in top_games:
+    img = i.find_all("img")
+    name = i.find_all(attrs={"class": "WsMG1c"})
+    pub = i.find_all(attrs={"class": "KoLSrc"})
+    package = i.find_all(attrs={"class": "poRVub"})
+    obj3 = TopGames(app_img=img[0]["data-src"], app_name=name[0].text, app_pub=pub[0].text[:20],
+              app_package=package[0]["href"][23:])
+    obj3.put()
 
 
 class HelloWebapp2(webapp2.RequestHandler):
@@ -64,6 +109,7 @@ class HelloWebapp2(webapp2.RequestHandler):
         border-radius: 25px; 
         }
         #c{
+        font-family: Georgia, serif;
         color:#b1cdcd;
         text-align: center;
         font-size: 40px;
@@ -73,54 +119,25 @@ class HelloWebapp2(webapp2.RequestHandler):
         text-shadow: 2px 2px 4px #000000;
         }
         #d{
-        margin-left: 90px;
-        color:#507c7c;
+        font-family: "Trebuchet MS", Helvetica, sans-serif;
+        margin-left: 20px;
+        color: white;
         font-size: 30px;
         padding-left: 50px;
         }
         #e{
-        margin-left: 90px;
-        color:#507c7c;
+        font-family: "Trebuchet MS", Helvetica, sans-serif;
+        margin-left: 20px;
+        color:white;
         font-size: 30px;
         padding-left: 50px;
         }
         #f{
-        margin-left: 90px;
-        color:#507c7c;
+        font-family: "Trebuchet MS", Helvetica, sans-serif;
+        margin-left: 20px;
+        color:white;
         font-size: 30px;
         padding-left: 50px;
-        }
-        #app1{
-        margin-left: 200px;
-        margin-right: 200px;
-        float:right;
-        text-align:top;
-        border-width:2px;
-        border-color:red;height:150px;
-        width : 150px;
-        color : white;
-        text-align: center;
-        }
-        #app2{
-        margin-left: 200px;
-        float:right;
-        text-align:top;
-        border-width:2px;
-        border-color:red;
-        height:150px;
-        width : 150px;
-        color : white;
-        text-align: center;
-        }
-        #app3{
-        float:right;
-        text-align:top;
-        border-width:2px;
-        border-color:red;
-        height:150px;
-        width : 150px;
-        color : white;
-        text-align: center;
         }
         #app1_img{
         border-radius: 20%;
@@ -131,6 +148,87 @@ class HelloWebapp2(webapp2.RequestHandler):
         #app3_img{
         border-radius: 20%;
         }
+        #app4_img{
+        border-radius: 20%;
+        }
+        #app5_img{
+        border-radius: 20%;
+        }
+        #app6_img{
+        border-radius: 20%;
+        }
+        #app1{
+	margin-left: 30px;
+	margin-right: 150px;
+	float:right;
+	text-align:top;
+	border-width:2px;
+	border-color:red;
+	height:150px;
+	width : 150px;
+	color : white;
+	text-align: center;
+	
+}
+#app2{
+	margin-left: 30px;
+	float:right;
+	text-align:top;
+	border-width:2px;
+	border-color:red;
+	height:150px;
+	width : 150px;
+	color : white;
+	text-align: center;
+	border-radius: 25px;
+}
+#app3{
+	margin-left: 30px;
+	float:right;
+	text-align:top;
+	border-width:2px;
+	border-color:red;
+	height:150px;
+	width : 150px;
+	color : white;
+	text-align: center;
+	border-radius: 20px;
+}
+#app4{
+	margin-left: 30px;
+	float:right;
+	text-align:top;
+	border-width:2px;
+	border-color:red;
+	height:150px;
+	width : 150px;
+	color : white;
+	text-align: center;
+	border-radius: 20px;
+}
+#app5{
+	margin-left: 30px;
+	float:right;
+	text-align:top;
+	border-width:2px;
+	border-color:red;
+	height:150px;
+	width : 150px;
+	color : white;
+	text-align: center;
+	border-radius: 20px;
+}
+#app6{
+	float:right;
+	text-align:top;
+	border-width:2px;
+	border-color:red;
+	height:150px;
+	width : 150px;
+	color : white;
+	text-align: center;
+	border-radius: 20px;
+}
         </style>
         <title>WebApp</title>
 </head>
@@ -139,63 +237,158 @@ class HelloWebapp2(webapp2.RequestHandler):
 <h1 id="b"><a href="https://www.bluestacks.com/" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/en/0/04/BlueStacks_Logo.png" width="240px" height="65px"></a></h1>
 <h2 id="c">Top Charts</h2><br>
 <div id = "d">Top Free Apps</div><br>""")
-        applications = App.all()
+        applications = TopFree.all()
         c = 0
-        for apps in applications:
+        for app in applications:
             if c == 0:
                 self.response.write('<p id = "app1">')
                 self.response.write(
-                    '<img id = "app1_img" src = "%s" width = "120px" height="120px"><br><br>' % apps.app_img)
-                self.response.write('%s<br>' % apps.app_name)
-                # 			self.response.write('%s<br>' %app.app_pub)
+                    '<img id = "app1_img" src = "%s" width = "120px" height="120px"><br><br>' % app.app_img)
+                self.response.write('%s<br>' % app.app_name)
+                self.response.write('%s<br>' %app.app_pub)
                 self.response.write('</p>')
             elif c == 1:
                 self.response.write('<p id = "app2">')
                 self.response.write(
-                    '<img id = "app2_img" src = "%s" width = "120px" height="120px"><br><br>' % apps.app_img)
-                self.response.write('%s<br>' % apps.app_name)
-                # 			self.response.write('%s<br>' %app.app_pub)
+                    '<img id = "app2_img" src = "%s" width = "120px" height="120px"><br><br>' % app.app_img)
+                self.response.write('%s<br>' % app.app_name)
+                self.response.write('%s<br>' %app.app_pub)
                 self.response.write('</p>')
             elif c == 2:
                 self.response.write('<p id = "app3">')
                 self.response.write(
-                    '<img id = "app3_img" src = "%s" width = "120px" height="120px"><br><br>' % apps.app_img)
-                self.response.write('%s<br>' % apps.app_name)
-                # 			self.response.write('%s<br>' %app.app_pub)
+                    '<img id = "app3_img" src = "%s" width = "120px" height="120px"><br><br>' % app.app_img)
+                self.response.write('%s<br>' % app.app_name)
+                self.response.write('%s<br>' %app.app_pub)
+                self.response.write('</p>')
+            elif c == 3:
+                self.response.write('<p id = "app4">')
+                self.response.write(
+                    '<img id = "app4_img" src = "%s" width = "120px" height="120px"><br><br>' % app.app_img)
+                self.response.write('%s<br>' % app.app_name)
+                self.response.write('%s<br>' %app.app_pub)
+                self.response.write('</p>')
+            elif c == 4:
+                self.response.write('<p id = "app5">')
+                self.response.write(
+                    '<img id = "app5_img" src = "%s" width = "120px" height="120px"><br><br>' % app.app_img)
+                self.response.write('%s<br>' % app.app_name)
+                self.response.write('%s<br>' %app.app_pub)
+                self.response.write('</p>')
+            elif c == 5:
+                self.response.write('<p id = "app6">')
+                self.response.write(
+                    '<img id = "app6_img" src = "%s" width = "120px" height="120px"><br><br>' % app.app_img)
+                self.response.write('%s<br>' % app.app_name)
+                self.response.write('%s<br>' %app.app_pub)
                 self.response.write('</p>')
             else:
                 break
             c += 1
         self.response.write("""
-        <br><br><br><br><br><br><br><br><br><br><br><br>
-        <div id = "e">Top Reviewed</div><br><br>
-        <p id = "app1">
-        <img id = "app1_img" src = "" width = "120px" height="120px"><br><br>
-        <br>
-        </p>
-        <p id = "app2">
-        <img id = "app2_img" src = "" width = "120px" height="120px"><br><br>
-        <br>
-        </p>
-        <p id = "app3">
-        <img id = "app3_img" src = "" width = "120px" height="120px"><br><br>
-        <br>
-        </p>
-        <br><br><br><br><br><br><br><br><br><br><br><br>
-        <div id = "f">Top Rated</div><br><br>
-        <p id = "app1">
-        <img id = "app1_img" src = "" width = "90px" height="90px"><br><br>
-        <br>
-        </p>
-        <p id = "app2">
-        <img id = "app2_img" src = "" width = "90px" height="90px"><br><br>
-        <br>
-        </p>
-        <p id = "app3">
-        <img id = "app3_img" src = "" width = "90px" height="90px"><br><br>
-        <br>
-        </p>
-</body>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div id = "e">Top Grossing</div><br><br>""")
+        top_gross = TopGross.all()
+        c = 0
+        for gross in top_gross:
+            if c==0:
+                self.response.write('<p id = "app1">')
+                self.response.write(
+                    '<img id = "app1_img" src = "%s" width = "120px" height="120px"><br><br>' % gross.app_img)
+                self.response.write('%s<br>' % gross.app_name)
+                self.response.write('%s<br>' %gross.app_pub)
+                self.response.write('</p>')
+            elif c == 1:
+                self.response.write('<p id = "app2">')
+                self.response.write(
+                    '<img id = "app2_img" src = "%s" width = "120px" height="120px"><br><br>' % gross.app_img)
+                self.response.write('%s<br>' % gross.app_name)
+                self.response.write('%s<br>' %gross.app_pub)
+                self.response.write('</p>')
+            elif c == 2:
+                self.response.write('<p id = "app3">')
+                self.response.write(
+                    '<img id = "app3_img" src = "%s" width = "120px" height="120px"><br><br>' % gross.app_img)
+                self.response.write('%s<br>' % gross.app_name)
+                self.response.write('%s<br>' %gross.app_pub)
+                self.response.write('</p>')
+            elif c == 3:
+                self.response.write('<p id = "app4">')
+                self.response.write(
+                    '<img id = "app4_img" src = "%s" width = "120px" height="120px"><br><br>' % gross.app_img)
+                self.response.write('%s<br>' % gross.app_name)
+                self.response.write('%s<br>' %gross.app_pub)
+                self.response.write('</p>')
+            elif c == 4:
+                self.response.write('<p id = "app5">')
+                self.response.write(
+                    '<img id = "app5_img" src = "%s" width = "120px" height="120px"><br><br>' %gross.app_img)
+                self.response.write('%s<br>' % gross.app_name)
+                self.response.write('%s<br>' %gross.app_pub)
+                self.response.write('</p>')
+            elif c == 5:
+                self.response.write('<p id = "app6">')
+                self.response.write(
+                    '<img id = "app6_img" src = "%s" width = "120px" height="120px"><br><br>' % gross.app_img)
+                self.response.write('%s<br>' % gross.app_name)
+                self.response.write('%s<br>' %gross.app_pub)
+                self.response.write('</p>')
+            else:
+                break
+            c += 1
+        self.response.write("""
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div id = "f">Top Games</div><br><br>""")
+        games = TopGames.all()
+        c = 0
+        for game in games:
+            if c==0:
+                self.response.write('<p id = "app1">')
+                self.response.write(
+                    '<img id = "app1_img" src = "%s" width = "120px" height="120px"><br><br>' % game.app_img)
+                self.response.write('%s<br>' % game.app_name)
+                self.response.write('%s<br>' %game.app_pub)
+                self.response.write('</p>')
+            elif c == 1:
+                self.response.write('<p id = "app2">')
+                self.response.write(
+                    '<img id = "app2_img" src = "%s" width = "120px" height="120px"><br><br>' % game.app_img)
+                self.response.write('%s<br>' % game.app_name)
+                self.response.write('%s<br>' %game.app_pub)
+                self.response.write('</p>')
+            elif c == 2:
+                self.response.write('<p id = "app3">')
+                self.response.write(
+                    '<img id = "app3_img" src = "%s" width = "120px" height="120px"><br><br>' % game.app_img)
+                self.response.write('%s<br>' % game.app_name)
+                self.response.write('%s<br>' %game.app_pub)
+                self.response.write('</p>')
+            elif c == 3:
+                self.response.write('<p id = "app4">')
+                self.response.write(
+                    '<img id = "app4_img" src = "%s" width = "120px" height="120px"><br><br>' % game.app_img)
+                self.response.write('%s<br>' % game.app_name)
+                self.response.write('%s<br>' %game.app_pub)
+                self.response.write('</p>')
+            elif c == 4:
+                self.response.write('<p id = "app5">')
+                self.response.write(
+                    '<img id = "app5_img" src = "%s" width = "120px" height="120px"><br><br>' % game.app_img)
+                self.response.write('%s<br>' % game.app_name)
+                self.response.write('%s<br>' %game.app_pub)
+                self.response.write('</p>')
+            elif c == 5:
+                self.response.write('<p id = "app6">')
+                self.response.write(
+                    '<img id = "app6_img" src = "%s" width = "120px" height="120px"><br><br>' % game.app_img)
+                self.response.write('%s<br>' % game.app_name)
+                self.response.write('%s<br>' %game.app_pub)
+                self.response.write('</p>')
+            else:
+                break
+            c += 1
+        self.response.write("""
+        </body>
 </html>""")
 
 
